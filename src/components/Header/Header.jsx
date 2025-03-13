fe00fca67ded7ebefa215c6ee7c30f20c880576f
LenaMaistrenko <istlauder@gmail.com>
2025-03-13 12:05:19 +0200

    navigate to PUBLIC_URL on logo click

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { Search } from "./Search/Search";
 export default function Header() {
   return (
     <header>
-      <a href="/">
+      <a href={`${process.env.PUBLIC_URL}`}>
         <img src={`${process.env.PUBLIC_URL}/img/logo-sephora.png`} alt="Sephora logo" className="logo"/>
         <img src={`${process.env.PUBLIC_URL}/img/logo-sephora-mobile.png`} alt="Sephora logo" className="mobile-logo"/>
       </a>
